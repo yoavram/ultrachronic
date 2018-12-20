80fa3bb484d030b84996f35de96ab7685e3a82fa
Yoav Ram <yoav@yoavram.com>
2018-12-20 10:00:08 +0200

    dont convert readme to rst

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 import warnings
-
 import versioneer
 
 readme = ''
@@ -10,24 +9,6 @@ try:
         readme = f.read()
 except FileNotFoundError:
     warnings.warn("README not found")
-else:
-    # try to convert README to rst, only works with internet connection and with requests installed
-    try:
-        import requests
-        r = requests.post(
-            url='http://c.docverter.com/convert',
-            data={'to':'rst', 'from': 'markdown'},
-            files={'input_files[]': open('README.md','rb')}
-        )
-        assert r.ok    
-    except ImportError:
-        warnings.warn("Failed importing requests, README will not be converted to rst")
-    except ConnectionError:
-        warnings.warn("Failed to connect to docverter.com")
-    except AssertionError:
-        warnings.warn("Bad response: {}".format(r.reason))
-    else:
-        readme = r.content.decode('utf8')
 
 setup(
     name='ultrachronic',
@@ -56,9 +37,6 @@ setup(
             'click>=5',
             'nose'
         ],
-        'dist': [
-            'requests'
-        ]
     }
 )
 
